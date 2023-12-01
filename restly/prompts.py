b126be87aa39407ffda7c80416fce54ea722211c
Marius Buleandra <mbuleandra@gmail.com>
2023-12-01 11:25:49 -0800

    update prompt

--- restly/prompts.py
+++ restly/prompts.py
@@ -1,4 +1,6 @@
-RELEVANT_APIS_PROMPT = """You are writing a tutorial to demonstrate the use of the set of APIs described below.
+RELEVANT_APIS_PROMPT = """You are an experienced product manager with 10+ years of experience.
+You are an expert when it comes to REST APIs.
+You are writing a tutorial to demonstrate the use of the set of APIs described below.
 This is what you need to cover in the tutorial: {query}
 Out of the APIs described in the spec below, what is a list of top 5 most relevant endpoints you can use?
 List all the relevant APIs, in a JSON format, with the following structure:
@@ -8,9 +10,19 @@ List all the relevant APIs, in a JSON format, with the following structure:
 """
 
 
-GENERATE_TUTORIAL_PROMPT = """You are writing a tutorial to demonstrate the use of the set of APIs described below.
-This is what you need to cover in the tutorial: {query}
-Return the tutorial content in markdown format.
+GENERATE_TUTORIAL_PROMPT = """You are an experienced developer with 10+ years of experience.
+You have extensive knowledge of REST APIs and current programming practices. 
+You tasked with writing a comprehensive, step-by-step tutorial for developers on how to use of the set of APIs described in the OpenAPI Spec provided.
+Your audience consists of developers with varying levels of experience, from beginners to advanced
+You must follow these guidelines when creating the tutorial:
+- Format: The tutorial must be written in markdown format.
+- API Focus: Strictly use only the APIs in the provided OpenAPI Spec. Do not use any other APIs or SDK clients.
+- Practicality: The tutorial must be written in a way that a developer can follow it and use the APIs. Offer guidance on how to prepare and use data effectively with these APIs.
+- Numbered Steps: Every step in the tutorial will be numbered for clarity and easy reference.
 
+TUTORIAL DESCRIPTION:
+{query}
+
+OpenAPI SPEC:
 {spec}
 """
